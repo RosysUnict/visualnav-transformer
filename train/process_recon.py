a246b351d97ca87cefdf7985df492fc993d7c275
fracance <fracance@gmail.com>
2024-02-13 17:39:22 +0100

    added multithreading to recon data processing

--- train/process_recon.py
+++ train/process_recon.py
@@ -6,6 +6,35 @@ import io
 import argparse
 import tqdm
 
+import multiprocessing as mp
+
+
+def process_traj(filename, recon_dir, output_dir):
+    # extract the name without the extension
+    traj_name = filename.split(".")[0]
+    # load the hdf5 file
+    try:
+        h5_f = h5py.File(os.path.join(recon_dir, filename), "r")
+    except OSError:
+        print(f"Error loading {filename}. Skipping...")
+        return
+    # extract the position and yaw data
+    position_data = h5_f["jackal"]["position"][:, :2]
+    yaw_data = h5_f["jackal"]["yaw"][()]
+
+    # save the data to a dictionary
+    traj_data = {"position": position_data, "yaw": yaw_data, "gps": gps}
+    traj_folder = os.path.join(output_dir, traj_name)
+    os.makedirs(traj_folder, exist_ok=True)
+    with open(os.path.join(traj_folder, "traj_data.pkl"), "wb") as f:
+        pickle.dump(traj_data, f)
+    # make a folder for the file
+    if not os.path.exists(traj_folder):
+        os.makedirs(traj_folder)
+    # save the image data to disk
+    for i in range(h5_f["images"]["rgb_left"].shape[0]):
+        img = Image.open(io.BytesIO(h5_f["images"]["rgb_left"][i]))
+        img.save(os.path.join(traj_folder, f"{i}.jpg"))
 
 def main(args: argparse.Namespace):
     recon_dir = os.path.join(args.input_dir, "recon_release")
@@ -20,33 +49,14 @@ def main(args: argparse.Namespace):
     if args.num_trajs >= 0:
         filenames = filenames[: args.num_trajs]
 
+    processes = []
     # processing loop
     for filename in tqdm.tqdm(filenames, desc="Trajectories processed"):
-        # extract the name without the extension
-        traj_name = filename.split(".")[0]
-        # load the hdf5 file
-        try:
-            h5_f = h5py.File(os.path.join(recon_dir, filename), "r")
-        except OSError:
-            print(f"Error loading {filename}. Skipping...")
-            continue
-        # extract the position and yaw data
-        position_data = h5_f["jackal"]["position"][:, :2]
-        yaw_data = h5_f["jackal"]["yaw"][()]
-        # save the data to a dictionary
-        traj_data = {"position": position_data, "yaw": yaw_data}
-        traj_folder = os.path.join(output_dir, traj_name)
-        os.makedirs(traj_folder, exist_ok=True)
-        with open(os.path.join(traj_folder, "traj_data.pkl"), "wb") as f:
-            pickle.dump(traj_data, f)
-        # make a folder for the file
-        if not os.path.exists(traj_folder):
-            os.makedirs(traj_folder)
-        # save the image data to disk
-        for i in range(h5_f["images"]["rgb_left"].shape[0]):
-            img = Image.open(io.BytesIO(h5_f["images"]["rgb_left"][i]))
-            img.save(os.path.join(traj_folder, f"{i}.jpg"))
-
+        p = mp.Process(target=process_traj, args=(filename, recon_dir, output_dir))
+        processes.append(p)
+        p.start()
+    for process in processes:
+        process.join()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
