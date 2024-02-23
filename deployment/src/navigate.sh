fde1d3ba5bd9d5f53b23ffa1a1634b5bbe662021
fracance <fracance@gmail.com>
2024-02-23 13:08:01 +0100

    Modified navigate.sh tmux session

--- deployment/src/navigate.sh
+++ deployment/src/navigate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a new tmux session
-session_name="vint_locobot_$(date +%s)"
+session_name="vint_rosys_$(date +%s)"
 tmux new-session -d -s $session_name
 
 # Split the window into four panes
@@ -12,26 +12,36 @@ tmux splitw -v -p 50 # split it into two halves
 
 tmux selectp -t 2    # select the new, second (2) pane
 tmux splitw -v -p 50 # split it into two halves
-tmux selectp -t 0    # go back to the first pane
 
-# Run the roslaunch command in the first pane
+tmux selectp -t 1    # select the  pane
+tmux splitw -v -p 50 # split it into two halves
+
+
+
 tmux select-pane -t 0
-tmux send-keys "roslaunch vint_locobot.launch" Enter
+tmux send-keys "ssh rosys@192.168.0.100" Enter
+tmux send-keys "roslaunch usb_cam usb_cam-test.launch" Enter
 
-# Run the navigate.py script with command line args in the second pane
+
 tmux select-pane -t 1
-# tmux send-keys "conda activate vint_deployment" Enter
+tmux send-keys "ssh rosys@192.168.0.100" Enter
+tmux send-keys "rosrun topic_tools throttle messages /usb_cam/image_raw 4.0 /image_throttle" Enter
+
+tmux select-pane -t 2
+tmux send-keys "conda activate vint_deployment_11" Enter
+tmux send-keys "cd ~/vint_ws/src/visualnav-transformer/deployment/src" Enter
 tmux send-keys "python navigate.py $@" Enter
 
-# Run the teleop.py script in the third pane
-tmux select-pane -t 2
-# tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python joy_teleop.py" Enter
-
-# Run the pd_controller.py script in the fourth pane
 tmux select-pane -t 3
-tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python pd_controller.py" Enter
+tmux send-keys "conda activate vint_deployment_11" Enter
+tmux send-keys "cd ~/vint_ws/src/visualnav-transformer/deployment/src" Enter
+tmux send-keys "python pd_controller.py"
+
+tmux select-pane -t 4
+tmux send-keys "conda activate vint_deployment_11" Enter
+tmux send-keys "cd ~/vint_ws/src/visualnav-transformer/deployment/src" Enter
+tmux send-keys "python visualize_plot_traj.py" Enter
+
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
