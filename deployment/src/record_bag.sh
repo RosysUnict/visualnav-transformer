b5663e9f49a1d3d62fd13c7b259e70a4cdc81a1e
fracance <fracance@gmail.com>
2024-02-28 11:35:25 +0100

    Modified tmux sessions for rosbag record and topomap creation

--- deployment/src/record_bag.sh
+++ deployment/src/record_bag.sh
@@ -7,20 +7,22 @@ tmux new-session -d -s $session_name
 # Split the window into three panes
 tmux selectp -t 0    # select the first (0) pane
 tmux splitw -v -p 50 # split it into two halves
-tmux selectp -t 0    # go back to the first pane
-tmux splitw -h -p 50 # split it into two halves
+# tmux selectp -t 0    # go back to the first pane
+# tmux splitw -h -p 50 # split it into two halves
 
 # Run the roslaunch command in the first pane
 tmux select-pane -t 0
-tmux send-keys "roslaunch vint_locobot.launch" Enter
+tmux send-keys "ssh rosys@192.168.0.100" Enter
+tmux send-keys "roslaunch usb_cam usb_cam-test.launch" Enter
+# tmux send-keys "roslaunch vint_locobot.launch" Enter
 
 # Run the teleop.py script in the second pane
-tmux select-pane -t 1
-tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python joy_teleop.py" Enter
+# tmux select-pane -t 1
+# tmux send-keys "conda activate vint_deployment" Enter
+# tmux send-keys "python joy_teleop.py" Enter
 
 # Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
-tmux select-pane -t 2
+tmux select-pane -t 1
 tmux send-keys "cd ../topomaps/bags" Enter
 tmux send-keys "rosbag record /usb_cam/image_raw -o $1" # change topic if necessary
 
